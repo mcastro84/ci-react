1c7bc94898031f37fa8eb28d601d6b96325a35c7
mcastro84 <decastro84@gmail.com>
2019-04-12 13:41:57 +0000

    Test will fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from 'react-testing-library'; 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render the text', () => {
+		const { getByText } = render(<App />);
+		expect(getByText('Edit').toBeTruthy());
 });
